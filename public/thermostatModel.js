85221721c58f54b4dc43c56fd1d33acdbbc8930e
cpcwood <cpcwood@outlook.com>
2020-01-22 10:08:53 +0000

    Min max powersaving modes added to model, no buttosn implemented q

--- public/thermostatModel.js
+++ public/thermostatModel.js
@@ -1,3 +1,18 @@
 function ThermostatModel(){
+  var min = 10;
+  this.powerSaving = true;
   this.temperature = 20;
+
+  this.increase = function(){
+    if (this.temperature < this._maxTemp()){ this.temperature++; }
+  };
+  this.decrease = function(){
+    if (this.temperature > min){ this.temperature--; }
+  }
+  this.togglePowerSaving = function(){
+    this.powerSaving = !this.powerSaving;
+  }
+  this._maxTemp = function(){
+    return (this.powerSaving ? 25 : 32);
+  }
 }
--- spec/frontend/unit/thermostatModelSpec.js
+++ spec/frontend/unit/thermostatModelSpec.js
@@ -20,4 +20,41 @@ describe("Thermostat Model", function() {
       expect(thermostatModel.temperature).toEqual(20);
     });
   });
+
+
+  describe("#increase", function() {
+    it("Increases temperature property by 1", function() {
+      thermostatModel.increase();
+      expect(thermostatModel.temperature).toEqual(21);
+    });
+    it("Powersaving mode is on by default", function() {
+      expect(thermostatModel.powerSaving).toEqual(true);
+    });
+    it("Powersaving mode is can be toggled", function() {
+      thermostatModel.togglePowerSaving();
+      expect(thermostatModel.powerSaving).toEqual(false);
+    });
+    it("Max temperature is 25 if powersaving mode on", function() {
+      for (var i = 0; i < 10; i++) thermostatModel.increase();
+      expect(thermostatModel.temperature).toEqual(25);
+    });
+    it("Max temperature is 32 if powersaving mode off", function() {
+      thermostatModel.togglePowerSaving();
+      for (var i = 0; i < 15; i++) thermostatModel.increase();
+      expect(thermostatModel.temperature).toEqual(32);
+    });
+
+  });
+
+  describe("#decrease", function() {
+    it("Decreases temperature property by 1", function() {
+      thermostatModel.decrease();
+      expect(thermostatModel.temperature).toEqual(19);
+    });
+    it("Has minimum temperature limit of 10", function() {
+      for (var i = 0; i < 11; i++) thermostatModel.decrease();
+      expect(thermostatModel.temperature).toEqual(10);
+    });
+  });
+
 });
